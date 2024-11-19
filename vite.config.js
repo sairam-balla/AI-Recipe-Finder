36ef6c623a4ea5eb8ff7f44e4610bd5fbb8c4759
Sairam Balla <141431338+sairam-balla@users.noreply.github.com>
2024-11-19 12:18:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
